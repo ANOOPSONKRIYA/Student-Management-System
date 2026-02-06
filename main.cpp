c6b326353ab6c45ca37b30a9c970701562ec8f37
ANOOP SONKRIYA <anoopsonkriya024@gmail.com>
2026-02-06 17:52:46 +0530

    Implemented Student class input and display functionality

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "student.h"
 using namespace std;
 
 int main() {
-    cout << "Student Management System" << endl;
+    Student s;
+
+    s.input();
+    s.display();
+
     return 0;
 }
--- student.cpp
+++ student.cpp
@@ -3,11 +3,43 @@
 using namespace std;
 
 void Student::input() {
-    // will implement in Phase 1
+    cout<<"Enter your roll number: ";
+    cin>>rollNo;
+
+    cin.ignore();  // VERY IMPORTANT
+    cout << "Enter Name: ";
+    getline(cin, name);
+
+    cout << "Enter Marks: ";
+    cin >> marks;
+
+    //simple grading logic
+    if (marks>=90)
+    {
+        grade = 'A';
+    } else if (marks>=80)
+    {
+        grade = 'B';
+    } else if (marks>=70)
+    {
+        grade = 'C';
+    } else if (marks>=60)
+    {
+        grade = 'D';
+    } else
+    {
+        grade = 'F';
+    }    
 }
 
 void Student::display() const {
-    // will implement in Phase 1
+    cout << "-----------------------" << endl;
+    cout << "Student Details -" << endl;
+    cout << "Roll No : " << rollNo << endl;
+    cout << "Name    : " << name << endl;
+    cout << "Marks  : " << marks << endl;
+    cout << "Grade  : " << grade << endl;
+    cout << "-----------------------" << endl;
 }
 
 int Student::getRollNo() const {
